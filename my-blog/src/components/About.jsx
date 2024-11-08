c8a190ffb010eaa520fdb9cf6be7c953631b20ac
Ling Qing Meng <lingqingmeng@Lings-MacBook-Pro.local>
2024-11-08 06:48:56 -0500

    recents

--- my-blog/src/components/About.jsx
+++ my-blog/src/components/About.jsx
@@ -2,7 +2,23 @@ function About() {
     return (
       <div>
         <h2 className="text-2xl font-bold mb-6">About</h2>
-        <p>About content coming soon...</p>
+        <p>Jason Meng is an etablished engineer, builder and founder. He has been building and investing in web3 startups since 2017.
+
+        He specialized in cybersecurity, creating a cybersecurity auditing firm that has worked with many successful projects including: </p>
+        <br/>
+        <ul> 
+          <li>●  Quantstamp</li>
+          <li>●  Swissborg</li>
+          <li>●  Machine Zone</li>
+          <li>●  FanVestor</li>
+          <li>●  Digimax</li>
+          <li>●  and many more</li>
+        </ul>
+        <br/>
+        <p>
+        To this date (last updated Nov 7th 2024) none of his clients have ever gotten hacked. He is also starring in a blockchain reality show called Angels Villa. A show about 10 founders competing to build the next innovative multi-billion dollar company. He is one of the experts and contestants on the show. Preview can be viewed on Instgram. 
+
+        </p>
       </div>
     );
   }
--- my-blog/src/components/Art.jsx
+++ my-blog/src/components/Art.jsx
@@ -2,7 +2,33 @@ function Art() {
     return (
       <div>
         <h2 className="text-2xl font-bold mb-6">Art</h2>
-        <p>Art content coming soon...</p>
+        <h3 className="text-xl font-semibold mt-6 mb-3"> Bio: </h3>
+        <div>Jason Meng</div>
+        <div> Angel Investor, Speaker, Engineer </div>
+        <div>
+          <a href="https://twitter.com/lingqingm" className="text-blue-600 underline hover:text-blue-800">Twitter</a>
+        </div>
+        <h3 className="text-xl font-semibold mt-6 mb-3"> Recent Talks </h3> 
+        <div>Todo: Update this section with ETH CC</div>
+        <figure className="max-w-[500px] w-full">
+          <img 
+            src="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*OQyZREFWnw8ZKhOVod-DsA.png" 
+            alt="Description of the image" 
+            className="w-full h-auto"
+          />
+          <figcaption className="text-gray-500 text-sm mt-2 text-center">
+            Jason Meng speaking at Ministry of Ideas on the next digital disruption: How blockchain and AI will transform Guest Experience in Hospitality. Conference Website: https://moi.lemiami.com/
+          </figcaption>
+        </figure>
+        <h3 className="text-xl font-semibold mt-6 mb-3"> Past Talks </h3>
+        <ol>
+          {/* add timestamp */}
+          <li>Ling Qing Meng speaking at Hacker Dojo Santa Clara: <a href="https://www.youtube.com/watch?v=aLC-bNitw7Y" className="text-blue-600 underline hover:text-blue-800">Link</a></li>
+          <li>Ling Qing Meng speaks at NYC Blockchain Center's first educational seminar: <a className="text-blue-600 underline hover:text-blue-800">Link</a></li>
+          <li>Ling Qing Meng speaking at ETH San Francisco, the recent, largest Hackathon for blockchain developers: <a className="text-blue-600 underline hover:text-blue-800">Link</a></li>
+          <li>Ling Qing Meng speaking at the WCEF blockchain conference: <a className="text-blue-600 underline hover:text-blue-800">Link</a></li>
+          <li>Ling Qing Meng coaching a Hackathon team to first place in Korea: <a href="https://github.com/decentral-inc/secret-voting" className="text-blue-600 underline hover:text-blue-800">Link</a></li>
+        </ol>
       </div>
     );
   }
