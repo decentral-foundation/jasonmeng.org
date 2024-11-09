839c93c7a33949567a2266859834cd8f52af1001
cco <cco@lingqingmeng.com>
2024-11-09 18:58:31 -0500

    next step is to remove talks

---
+++ my-blog/src/data/talks.js
@@ -0,0 +1,16 @@
+export const talks = [
+  {
+    id: "first-talk",
+    date: "Mar 24, 2021",
+    title: "First Talk",
+    description: "A talk about something interesting",
+    url: "https://localhost:5173/first-talk"
+  },
+  {
+    id: "second-talk",
+    date: "Apr 8, 2021",
+    title: "Second Talk",
+    description: "Another fascinating talk",
+    url: "https://localhost:5173/second-talk"
+  }
+]; 
