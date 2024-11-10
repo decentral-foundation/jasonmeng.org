0ffd97113c1a0cfca4b24fc9d36fe71c3625bb4e
cco <cco@lingqingmeng.com>
2024-11-09 19:00:21 -0500

    got rid of talks

--- my-blog/src/components/Posts.jsx
+++ my-blog/src/components/Posts.jsx
@@ -1,11 +1,10 @@
 import { Link } from 'react-router-dom';
 import { posts } from '../data/posts';
-import { talks } from '../data/talks';
 
 function Posts() {
   return (
     <div>
-      <section className="mb-12">
+      <section>
         <h2 className="text-2xl font-bold mb-6">Posts</h2>
         {posts.map((post) => (
           <div key={post.id} className="mb-4">
@@ -16,18 +15,6 @@ function Posts() {
             >
               {post.title}
             </Link>
-          </div>
-        ))}
-      </section>
-
-      <section>
-        <h2 className="text-2xl font-bold mb-6">Recent Talks / Interviews</h2>
-        {talks.map((talk) => (
-          <div key={talk.id} className="mb-4">
-            <span className="text-gray-500 mr-4">{talk.date}</span>
-            <a href={talk.url} className="text-blue-500 hover:underline">
-              {talk.title}
-            </a>
           </div>
         ))}
       </section>
