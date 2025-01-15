d4461647e96062a0ef68286f6da6c05fb838a561
Ling Qing Meng <lingqingmeng@Lings-MacBook-Air.local>
2025-01-15 16:47:01 -0500

    diff env files

--- my-blog/src/lucia.jsx
+++ my-blog/src/lucia.jsx
@@ -6,9 +6,9 @@ const lucia = {
     init: () => {
         console.log('About to initialize SDK');
         LuciaSDK.init({ 
-            clientId: process.env.VITE_CLIENT_ID,
-            baseURL: process.env.VITE_BASE_URL,
-            api_key: process.env.VITE_API_KEY,
+            clientId: import.meta.env.VITE_CLIENT_ID,
+            baseURL: import.meta.env.VITE_BASE_URL,
+            api_key: import.meta.env.VITE_API_KEY,
         });
         LuciaSDK.getClientIP();
       },
