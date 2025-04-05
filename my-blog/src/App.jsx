3106fdf991a1740db5c924de8495eaec73ae9be6
Ling Qing Meng <lingqingmeng@Lings-MacBook-Air.local>
2025-04-05 10:41:36 -0400

    tag mangager

--- my-blog/src/App.jsx
+++ my-blog/src/App.jsx
@@ -8,9 +8,29 @@ import Header from './components/Header';
 import './index.css';
 
 function App() {
+  const handleTestClick = () => {
+    if (window.dataLayer) {
+      window.dataLayer.push({
+        'event': 'test_button_click',
+        'category': 'Test',
+        'action': 'Click',
+        'label': 'GTM Test Button'
+      });
+      console.log('GTM test event pushed');
+    } else {
+      console.error('dataLayer not found');
+    }
+  };
+
   return (
     <BrowserRouter>
       <div className="container mx-auto px-4">
+        <button
+          onClick={handleTestClick}
+          className="fixed bottom-4 right-4 bg-blue-500 text-white px-4 py-2 rounded-md"
+        >
+          Test GTM
+        </button>
         <Header />
         <Routes>
           <Route path="/" element={<Posts />} />
